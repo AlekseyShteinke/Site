dad5379ff9e24480b5ab5647000a8f48067c0b4c
Pavel-Shtein <80421244+Pavel-Shtein@users.noreply.github.com>
2021-03-11 21:04:15 +0300

    Add files via upload

--- DjangoWebProject1/DjangoWebProject1/app/forms.py
+++ DjangoWebProject1/DjangoWebProject1/app/forms.py
@@ -16,3 +16,17 @@ class BootstrapAuthenticationForm(AuthenticationForm):
                                widget=forms.PasswordInput({
                                    'class': 'form-control',
                                    'placeholder':'Password'}))
+
+class ListSearch(forms.Form):
+    CHOICES=((0,'Любой'),(1,'1'),(2,'2'),(3,'3'),(4,'4'),(5,'5'),(6,'6'),(7,'7'),(8,'8'),(9,'9'),(10,'10'),(11,'11'))
+    school=forms.CharField(max_length=10,min_length=None,label="Школа", required=False,
+                           widget=forms.TextInput({
+                               'class':'form-control',
+                               'placeholder': 'Номер школы'}))
+    fio=forms.CharField(max_length=100,min_length=None,label="ФИО", required=False,
+                           widget=forms.TextInput({
+                               'class':'form-control',
+                               'placeholder': 'ФИО'}))
+    cls=forms.ChoiceField(choices=CHOICES, label='Класс',
+                           widget=forms.Select({
+                               'class':'form-control'}))
