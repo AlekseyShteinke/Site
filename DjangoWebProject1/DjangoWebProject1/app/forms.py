6c420c7993a5b6190d9e2eeef7b684febfa7003c
AlekseyShteinke <43885149+AlekseyShteinke@users.noreply.github.com>
2023-10-12 18:23:16 +0300

    Update forms.py

--- DjangoWebProject1/DjangoWebProject1/app/forms.py
+++ DjangoWebProject1/DjangoWebProject1/app/forms.py
@@ -12,11 +12,11 @@ class BootstrapAuthenticationForm(AuthenticationForm):
     username = forms.CharField(max_length=254,
                                widget=forms.TextInput({
                                    'class': 'form-control',
-                                   'placeholder': 'User name'}))
+                                   'placeholder': 'Имя пользователя'}))
     password = forms.CharField(label=_("Password"),
                                widget=forms.PasswordInput({
                                    'class': 'form-control',
-                                   'placeholder':'Password'}))
+                                   'placeholder':'Пароль'}))
 
 class ListSearch(forms.Form):
     CHOICES=((0,'Любой'),(1,'1'),(2,'2'),(3,'3'),(4,'4'),(5,'5'),(6,'6'),(7,'7'),(8,'8'),(9,'9'),(10,'10'),(11,'11'))
@@ -31,3 +31,21 @@ class ListSearch(forms.Form):
     cls=forms.ChoiceField(choices=CHOICES, label='Класс',
                            widget=forms.Select({
                                'class':'form-control'}))
+
+class CreateUser(forms.Form):
+    login = forms.CharField(max_length=20, min_length=None, label='Логин', required=True,
+                            widget=forms.TextInput({
+                                'class':'form-control',
+                                'placeholder':''}))
+    password  = forms.CharField(max_length=100, min_length=0, label='Пароль', required=True,
+                                widget=forms.PasswordInput({
+                                    'class':'form-control',
+                                    'placeholder':''}))
+    password_corr = forms.CharField(min_length=0, label='Повторите пароль', required=True,
+                                widget=forms.PasswordInput({
+                                    'class':'form-control',
+                                    'placeholder':''}))
+    fio = forms.CharField(max_length=100, min_length=0, label='ФИО', required=True,
+                          widget=forms.TextInput({
+                              'class':'form-control',
+                              'placeholder':''}))
